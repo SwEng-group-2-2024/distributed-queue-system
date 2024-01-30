678930073b19fdb0afffb8dfe01f03200ab362ad
camachof <camacholenihanfionn@gmail.com>
2024-01-30 15:32:53 +0000

    Setting up CI/CD for backend and frontend.

---
+++ backend/deployBackend.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env
+
+# install dependencies and deploy the API
+npm install
+node index.js
---
+++ frontend/__tests__/index.js
@@ -0,0 +1,3 @@
+it('Placeholder Test is... Testing.', () => {
+    expect(1 + 1).toEqual(2);
+})
