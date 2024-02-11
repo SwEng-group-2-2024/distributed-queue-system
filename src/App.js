e4b041c9e5aa81350d3534120f95f10329f1ee1e
Michael Ola <53217504+micholag3636@users.noreply.github.com>
2024-02-11 20:30:15 +0000

    Sign Up featurs Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SignUp from "./SignUp.js";
+import MainPage from "./MainPage.js"; 
+import { useSelector } from 'react-redux';
 
 function App() {
+  const isSignedUp = useSelector(state => state.isSignedUp); 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {isSignedUp ? <MainPage /> : <SignUp />}
+
+      
     </div>
   );
 }
---
+++ src/MainPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MainPage.css';
+
+function MainPage() {
+
+
+  return (
+    <div className="Main">
+    
+    </div>
+  );
+}
+
+export default MainPage;
