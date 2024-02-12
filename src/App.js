56bd6624cc174cdd86fcf41ecef0a65e864339c2
Alex-Rybak <30793837+Alex-Rybak@users.noreply.github.com>
2024-02-12 02:36:02 +0000

    finished login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import LoginPage from "./auth/Login.js"; 
+import store from './store.js';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={store}>
+        <LoginPage />
+      </Provider>
     </div>
   );
 }
--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,42 +1,57 @@
+import React, { useState } from 'react';
 import './Login.css';
-import React, { useState  }from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-const BASE_URL = 'http://localhost:3000';
+import { useDispatch } from 'react-redux';
+import { loginSuccess } from '../store.js';
+import logo from '../fence.png';
+const PORT = process.env.PORT || 4000;
 
 
-const LoginPage = () => {
-    const [newUser, setNewUser] = useState({ // defined user
-        username: '',
-        password: '',
-      });
-    // const handleRegister = async () => {} : function to handle the login
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setNewUser((prevUser) => ({
-          ...prevUser,
-          [name]: value,
-        }));
-      };
+function LoginPage ()  {
+   const dispatch = useDispatch();
+
+    const [email, setEmail] = useState('');
+    const [emailError, setEmailError] = useState('');
+
+    const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    
+    const handleLogin = async (e) => { // function to handle the login
+        try {
+            const response = await fetch('http://localhost:${PORT}/api/login', {
+              method: 'POST',
+              headers: {'Content-Type': 'application/json'},
+      
+              body: JSON.stringify({email, password}),
+            });
+            if (response.ok) {
+              dispatch(loginSuccess());
+            } else {
+              console.error('incorrect login or password:', response.statusText);
+            }
+          } catch (error) {
+            console.error('error logging in:', error);
+          }
+    } ;
 
     return (
-        <div>
-          <h1 className='login-page'>log in</h1>
-          <label>
-            username:
-            <input type="text" name="username" value={newUser.username} onChange={handleInputChange} />
-          </label>
-          <br></br>
-          <label>
-            password:
-            <input type="text" name="homeAddress" value={newUser.homeAddress} onChange={handleInputChange} />
-          </label>
-          <br></br>
-          <button onClick={handleRegister}>log in</button>
-          <div>
-            <strong>Message: </strong>
-            {message}
-          </div>
+      <div className="return">
+        <img src={logo} style={{ width: '500px', height: '382px' }} alt="Fence logo" />
+        <div className="login">
+        
+          <h1>log in</h1>
+          <form className="inputBox" onSubmit={handleLogin}>
+
+            <input placeholder="email" className="input" value={email} onChange={(e) => setEmail(e.target.value)} />
+            {emailError && <div className="error">{emailError}</div>}
+
+            <input placeholder="password" className="input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            {passwordError && <div className="error">{passwordError}</div>}
+
+            <button type="submit" className="submitBtn">log in</button>
+          </form>
         </div>
+      </div>
+      
       );
     };
 
