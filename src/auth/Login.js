c8941a67e08dc5b1f6dcf58ed76ae9fc7f39b03d
Alex-Rybak <30793837+Alex-Rybak@users.noreply.github.com>
2024-02-13 01:59:52 +0000

    made fixes to login

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -5,19 +5,20 @@ import { loginSuccess } from '../store.js';
 import logo from '../fence.png';
 const PORT = process.env.PORT || 4000;
 
-
 function LoginPage ()  {
    const dispatch = useDispatch();
 
     const [email, setEmail] = useState('');
-    const [emailError, setEmailError] = useState('');
 
     const [password, setPassword] = useState('');
     const [passwordError, setPasswordError] = useState('');
     
     const handleLogin = async (e) => { // function to handle the login
+      e.preventDefault();
+      setPasswordError('');
+
         try {
-            const response = await fetch('http://localhost:${PORT}/api/login', {
+            const response = await fetch('http://localhost:3000/api/login', {
               method: 'POST',
               headers: {'Content-Type': 'application/json'},
       
@@ -27,6 +28,7 @@ function LoginPage ()  {
               dispatch(loginSuccess());
             } else {
               console.error('incorrect login or password:', response.statusText);
+              setPasswordError('incorrect login or password');
             }
           } catch (error) {
             console.error('error logging in:', error);
@@ -38,11 +40,10 @@ function LoginPage ()  {
         <img src={logo} style={{ width: '500px', height: '382px' }} alt="Fence logo" />
         <div className="login">
         
-          <h1>log in</h1>
           <form className="inputBox" onSubmit={handleLogin}>
 
             <input placeholder="email" className="input" value={email} onChange={(e) => setEmail(e.target.value)} />
-            {emailError && <div className="error">{emailError}</div>}
+            
 
             <input placeholder="password" className="input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
             {passwordError && <div className="error">{passwordError}</div>}
--- src/store.js
+++ src/store.js
@@ -1,16 +1,13 @@
-import { createStore } from 'redux';
-
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
 
 const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 
-
 export const loginSuccess = () => ({ type: LOGIN_SUCCESS });
-
 
 const initialState = {
   isLoggedIn: false,
 };
-
 
 const loginReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -21,7 +18,13 @@ const loginReducer = (state = initialState, action) => {
   }
 };
 
+// Wrapping the loginReducer inside an object
+const rootReducer = combineReducers({
+  login: loginReducer,
+});
 
-const store = createStore(loginReducer);
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 export default store;
