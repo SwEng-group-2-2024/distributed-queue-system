261149e4638631e2045a8092b5c090314b621570
PowerUserE <125454293+PowerUserE@users.noreply.github.com>
2024-03-26 12:24:46 +0000

    updated VM 1 to base code (removed rabbitMQ implementation

--- DQS_01/dbService.js
+++ DQS_01/dbService.js
@@ -1,131 +1,77 @@
-const sql = require("mssql");
 const { getPool } = require("./database");
 const retryWithExponentialBackoff = require("./retryWithExponentialBackoff");
-const logger = require("./logger");
 
-// exports.createTask = async (task) => {
-//   console.log("Creating task...");
-//   const pool = await getDbPool();
-//   try {
-//     const result = await pool
-//       .request()
-//       .input("TaskData", sql.NVarChar, JSON.stringify(task)) // Assuming task is a JSON object
-//       .query(
-//         "INSERT INTO Tasks (TaskData) OUTPUT INSERTED.* VALUES (@TaskData)"
-//       );
-
-//     return result.recordset[0];
-//   } finally {
-//     pool.close();
-//   }
-// };
-
-exports.getTaskStatus = async (uniqueTaskID) => {
-  const pool = await getPool();
-  try {
-    const query = `SELECT Status FROM Tasks WHERE uniqueTaskID = '${uniqueTaskID}'`;
-    await retryWithExponentialBackoff(() => pool.request().query(query));
-    console.log("Task status updated successfully");
-  } catch (error) {
-    console.error("Error updating task status:", error);
-    throw error;
-  }
-};
-
-exports.updateTaskStatus = async (uniqueTaskID, status) => {
-  const pool = await getPool();
-  try {
-    const query = `UPDATE Tasks SET Status = '${status}' WHERE uniqueTaskID = '${uniqueTaskID}'`;
-    await retryWithExponentialBackoff(() => pool.request().query(query));
-    console.log("Task status updated successfully");
-  } catch (error) {
-    console.error("Error updating task status:", error);
-    throw error;
-  }
-};
-
-exports.createTask = async (task) => {
-  ////////////////////////////////////////
-  // To check if the provided timestamp is valid
-  const { message, timestamp, sender } = task;
-  let parsedTimestamp = timestamp;
+exports.enqueueMessage = async (inputMessage) => {
+  const { message, timestamp, sender } = inputMessage;
+  let parsedTimestamp;
 
   // Check if the provided timestamp is valid
   try {
-    // Attempt to parse the timestamp
-    const parsedDate = new Date(timestamp);
-
-    // If parsing succeeds, update the parsedTimestamp variable
-    parsedTimestamp = parsedDate.toISOString();
+    parsedTimestamp = new Date(timestamp).toISOString();
   } catch (error) {
     console.error("Invalid timestamp format. Using current timestamp instead.");
-
-    // If parsing fails, use the current timestamp
     parsedTimestamp = new Date().toISOString();
   }
-  ////////////////////////////////////////
 
-  // Generate a unique task ID using a hash function
-  const uniqueTaskID = generateUniqueTaskID(message, parsedTimestamp, sender);
+  // Generate a unique Message ID using a hash function
+  const uniqueMessageID = generateUniqueMessageID(
+    message,
+    parsedTimestamp,
+    sender
+  );
 
   const query = `
-      INSERT INTO Tasks (message, timestamp, sender, uniqueTaskID, status)
-      VALUES ('${message}', '${parsedTimestamp}', '${sender}', '${uniqueTaskID}', 'pending');
-    `;
+    INSERT INTO Tasks (message, sender, uniqueMessageID, timestamp)
+    VALUES ('${message}', '${sender}', '${uniqueMessageID}', '${parsedTimestamp}');
+  `;
+
   try {
     const pool = await getPool();
     await retryWithExponentialBackoff(() => pool.request().query(query));
-    const createdTask = {
+    const createdMessage = {
       message,
       timestamp: parsedTimestamp,
       sender,
-      uniqueTaskID,
-      status: "pending",
+      uniqueMessageID,
     };
-    return { task: createdTask };
+    return { message: createdMessage };
   } catch (error) {
-    console.log("Error creating task:", error);
-    throw new Error("Error creating task.");
+    console.log("Error enqueueing message:", error);
+    throw new Error("Error enqueueing message.");
   }
 };
 
-exports.getNextTask = async () => {
+exports.dequeueMessage = async () => {
   const pool = await getPool();
   try {
-    const query = `SELECT TOP 1 * FROM Tasks ORDER BY Timestamp ASC`;
+    const query = `SELECT TOP 1 * FROM tasks ORDER BY Timestamp ASC`;
+
     const result = await retryWithExponentialBackoff(() =>
       pool.request().query(query)
     );
 
     var resultString = JSON.stringify(result["recordset"]);
-
     var parsedResult = JSON.parse(resultString);
 
-    uniqueTaskID = parsedResult[0]["uniqueTaskID"];
+    uniqueMessageID = parsedResult[0]["uniqueMessageID"];
 
     if (result.recordset.length > 0) {
-      const nextTask = result.recordset[0];
-      if (this.getTaskStatus(uniqueTaskID) === "pending") {
-        // If the next task is pending, retry with exponential backoff
-        throw new Error("Next task is still pending");
-      } else {
-        // If the next task is not pending, return it
-        const query = `DELETE FROM Tasks WHERE uniqueTaskID = '${nextTask.uniqueTaskID}'`;
-        await retryWithExponentialBackoff(() => pool.request().query(query));
-        return nextTask;
-      }
+      const nextMessage = result.recordset[0];
+      const query = `DELETE FROM tasks WHERE uniqueMessageID = '${nextMessage.uniqueMessageID}'`;
+      await retryWithExponentialBackoff(() => pool.request().query(query));
+      return nextMessage;
     } else {
-      throw new Error("No tasks available");
+      throw new Error("No messages available");
     }
   } catch (error) {
-    console.error("Error fetching next task:", error);
+    console.error("Error fetching next message:", error);
     // Retry with exponential backoff
-    await retryWithExponentialBackoff(getNextTask);
+    await retryWithExponentialBackoff(dequeueMessage);
   }
 };
 
-// Helper function to generate a unique task ID using SHA-256 hashing
-function generateUniqueTaskID(message, timestamp, sender) {
+// Helper function to generate a unique Message ID using SHA-256 hashing
+function generateUniqueMessageID(message, timestamp, sender) {
   const crypto = require("crypto");
   const hash = crypto.createHash("sha256");
   hash.update(message + timestamp + sender);
--- DQS_01/logger.js
+++ DQS_01/logger.js
@@ -1,37 +1,9 @@
-const winston = require("winston");
+// const winston = require("winston");
 
 // const logger = winston.createLogger({
 //   level: "info",
 //   format: winston.format.json(),
-//   transports: [
-//     new winston.transports.Console(),
-//     // Add other transports such as file if required
-//   ],
+//   transports: [new winston.transports.Console()],
 // });
 
-// logger.js
-
-// const { createLogger, transports, format } = require("winston");
-
-// // Create a Winston logger instance
-// const logger = createLogger({
-//   level: "info", // Set the default log level
-//   format: format.combine(
-//     format.timestamp(), // Add timestamp to log messages
-//     format.json() // Log messages in JSON format
-//   ),
-//   transports: [
-//     new transports.Console(), // Log to the console
-//     new transports.File({ filename: "logfile.log" }), // Log to a file
-//   ],
-// });
-
-// // Export the logger instance
-
-const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.json(),
-  transports: [new winston.transports.Console()],
-});
-
-module.exports = logger;
+// module.exports = logger;
