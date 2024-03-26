6785fc0298af506aee6ad90d01a00a267c62ee6d
PowerUserE <125454293+PowerUserE@users.noreply.github.com>
2024-03-26 12:27:53 +0000

    updated VM 3 to base code (removed rabbitMQ implementation

--- DQS_03/routes.js
+++ DQS_03/routes.js
@@ -1,16 +1,8 @@
 const express = require("express");
-const taskController = require("./taskController");
+const controller = require("./controller");
 const router = express.Router();
-const logger = require("./logger");
-const cors = require("cors");
 
-router.use(cors());
-
-// Change this from "/tasks" to "/" to correctly handle requests to "/api/tasks"
-router.post("/tasks/enqueue", taskController.createTask);
-
-// Adjusted to handle GET requests at "/api/tasks/:taskId"
-router.get("/tasks/dequeue", taskController.getNextTask);
+router.post("/message/enqueue", controller.enqueueMessage);
+router.get("/message/dequeue", controller.dequeueMessage);
 
 module.exports = router;
-
